7c1591097a57e2c961b32bf57ae846cb134ea8fd
Seth Bonnie <sethkbonnie@gmail.com>
2019-03-18 20:44:40 -0500

    Add core component structure

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from '@emotion/styled';
+
+const Container = styled.div`
+  position: relative;
+  height: 100%;
+  width: 100%;
+  padding-right: 200px;
+  background: #ccc;
+`;
+
+const ContentContainer = styled.div`
+  position: relative;
+  height: 100%;
+  width: 100%;
+  overflow-y: auto;
+  background-color: #fff;
+  border: 1px solid red;
+`;
+
+const SideBar = styled.div`
+  position: absolute;
+  right: 0px;
+  top: 0px;
+  width: 200px;
+  height: 100%;
+  overflow-y: auto;
+  background-color: #999;
+`;
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Container>
+        <ContentContainer />
+        <SideBar />
+      </Container>
     );
   }
 }
